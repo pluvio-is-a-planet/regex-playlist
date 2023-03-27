b53bae6817360ebf7fa295cbe37be30d6cf9c0a2
Rynhardt Engelbrecht <107496937+pluvio-is-a-planet@users.noreply.github.com>
2023-03-27 17:26:53 +0200

    Add regex testing for all fields on input form
    Created a const variable to hold all regex patterns for corresponding
    input fields, then created a function that tests the content of the
    input field against the regex pattern, and adds a class to
    the corresponding input field that determines whether the user's input
    matches the regex pattern or not.

--- validation.js
+++ validation.js
@@ -1,1 +1,28 @@
 // validation script here
+const userInputs = document.querySelectorAll('input');      
+
+const regExPatterns = {
+    username: /^[a-z\d]{5,12}$/i,
+    email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/i,
+    telephone: /^(\d{11})$/,
+    password: /^[\w@-]{8,20}$/,
+    slug: /^[a-z\d-]{8,20}$/
+};
+
+function validate(field, regex){
+    console.log('field:', field);
+    console.log('regex:', regex);
+    if (regex.test(field.value)) {
+        field.className = 'valid';
+    } else {
+        field.className = 'invalid';
+    }
+}
+
+console.log(regExPatterns);
+
+userInputs.forEach((input) => {
+    input.addEventListener('keyup',(activeField) => {
+        validate(activeField.target, regExPatterns[activeField.target.attributes.name.value]);
+    })
+})
